579ba11d5b94561ba3c38d22a6f29ecc5b8ae8a1
Ryan Southard <eodchop@gmail.com>
2017-07-15 22:01:42 -0500

    fixed some shit

--- star-wars-rpg/assets/javascript/script.js
+++ star-wars-rpg/assets/javascript/script.js
@@ -1,7 +1,7 @@
 /**
  * Created by ryansouthard on 7/13/17.
  */
-var selectedCharcterId= "";
+var selectedCharcterId = "";
 var hasPickedCharacter = false;
 var hasPickedDefender = false;
 var selectedDefenderId = "";
@@ -9,44 +9,44 @@ var selectedDefenderId = "";
 //document on ready function
 $(function () {
     /* On character clicked (onclick event will be needed)
-    Move character to different div section*/
+     Move character to different div section*/
 
-        $(".character").on("click", function() {
-            console.log($("#character-row").children()[0]);
-            if (!hasPickedCharacter) {
-                console.log($("#character-row"));
-                movePlayerCharacter(this);
-                moveEnemies();
-            }else {
-                moveDefender(this);
-            }
-        })
+    $(".character").on("click", function () {
+        console.log($("#character-row").children()[0]);
+        if (!hasPickedCharacter) {
+            console.log($("#character-row"));
+            movePlayerCharacter(this);
+            moveEnemies();
+        } else {
+            moveDefender(this);
+        }
+    })
 })
 
-function movePlayerCharacter(playerDiv){
+function movePlayerCharacter(playerDiv) {
     $("#character-section").append(playerDiv);
     hasPickedCharacter = true;
     selectedCharcterId = playerDiv.id;
 }
-function moveEnemies(){
+function moveEnemies() {
     /*For every child in $("#character-row").children() we want to append that child to
      * enemies-avaiable-to-attack div
      */
     var charactersArray = $("#character-row").children();
-    for(var i = 0; i < charactersArray.length; i++){
+    for (var i = 0; i < charactersArray.length; i++) {
         $("#enemies-available-to-attack").append(charactersArray[i]);
     }
 }
 
-function moveDefender(defenderDiv){
+function moveDefender(defenderDiv) {
     /*move character clicked to fight section
-    */
+     */
     $("#defender").append(defenderDiv);
     hasPickedDefender = true;
     selectedDefenderId = defenderDiv.id;
 }
 
-function setHealth(){
+function setHealth() {
     obiwon: 100;
     luke: 100;
     darth: 100;
@@ -55,7 +55,7 @@ function setHealth(){
 
 }
 
-function setHitPoints(){
+function setHitPoints() {
 
 
 }
